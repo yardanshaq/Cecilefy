916a1c8894df52f5c0e51ab28175deb53fbdd426
Yardan <139103935+yardanshaq@users.noreply.github.com>
2025-09-20 17:48:04 +0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,13 @@
 // this configuration is for migrations only
-// and since jwt secret is not required, it's set to a placehodler string to bypass env validation
+// and since jwt secret is not required, it's set to a placeholder string to bypass env validation
 if (!process.env.JWT_SECRET) {
   process.env.JWT_SECRET = "securekey";
 }
 
 const env = require("./server/env");
 
-const isSQLite = env.DB_CLIENT === "sqlite3" || env.DB_CLIENT === "better-sqlite3";
+const isSQLite =
+  env.DB_CLIENT === "sqlite3" || env.DB_CLIENT === "better-sqlite3";
 
 module.exports = {
   client: env.DB_CLIENT,
@@ -17,12 +18,16 @@ module.exports = {
     user: env.DB_USER,
     port: env.DB_PORT,
     password: env.DB_PASSWORD,
-    ssl: env.DB_SSL,
+    ssl:
+      env.DB_SSL === "true"
+        ? { rejectUnauthorized: false }
+        : false,
+    family: 4, // 👉 force IPv4 biar gak error ENETUNREACH
   },
   useNullAsDefault: true,
   migrations: {
     tableName: "knex_migrations",
     directory: "server/migrations",
     disableMigrationsListValidation: true,
-  }
+  },
 };
